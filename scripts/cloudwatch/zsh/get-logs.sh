613f538a4eb9c0abb56d613c0289ed8b61c95ae5
Practical Serverless <57728518+johngranthamdev@users.noreply.github.com>
2023-05-05 10:57:30 -0700

    Updated the start-time argument in get-logs.sh

--- scripts/cloudwatch/zsh/get-logs.sh
+++ scripts/cloudwatch/zsh/get-logs.sh
@@ -2,7 +2,7 @@
 
 source config.sh
 
-start=$(date -v-3d +"%s")
+start=$(date -v-1d +"%s")
 
 api=$(aws cloudformation describe-stacks \
     --stack-name ${service}${environment} \
@@ -13,8 +13,8 @@ group="API-Gateway-Execution-Logs_${api}/api"
 pattern="request body before transformations"
 
 aws logs filter-log-events \
-   --log-group-name $group \
-   --start-time "${start}" \
+   --log-group-name ${group} \
+   --start-time ${start}000 \
    --filter-pattern "${pattern}" \
    --output text \
    --page-size 100
