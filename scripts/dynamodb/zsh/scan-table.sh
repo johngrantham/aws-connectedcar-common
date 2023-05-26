4a800ad7ab358e7b555348f900400badf0657c6c
Practical Serverless <57728518+johngranthamdev@users.noreply.github.com>
2023-05-26 11:22:44 -0700

    Changed the scan-table.sh script to use the numeric stateCode filter

--- scripts/dynamodb/zsh/scan-table.sh
+++ scripts/dynamodb/zsh/scan-table.sh
@@ -4,5 +4,5 @@ source config.sh
 
 aws dynamodb scan \
   --table-name ${service}_Dealer_Table_${environment} \
-  --expression-attribute-values '{":code":{"S":"OR"}}' \
+  --expression-attribute-values '{":code":{"N":"41"}}' \
   --filter-expression "stateCode = :code"
