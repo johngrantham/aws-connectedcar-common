8c2ea7e70e81f2d559fe44c5af477c7ae752a4c8
Practical Serverless <57728518+johngranthamdev@users.noreply.github.com>
2023-05-27 09:29:50 -0700

    added restore-table.sh script

---
+++ scripts/dynamodb/zsh/restore-table.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+source config.sh
+
+time=$(date -u -v-10M +"%Y-%m-%dT%H:%M:%S")
+
+aws dynamodb restore-table-to-point-in-time \
+  --source-table-name ${service}_Dealer_Table_${environment} \
+  --target-table-name ${service}_Dealer_Table_${environment}_Restored \
+  --restore-date-time ${time}
+
